414ab35a992956d41dcf16610df8be21c67b6036
Katie Russ <katie.k7r@gmail.com>
2017-08-07 20:53:35 -0400

    Starting on modals for footer content (calendar, contact, courses) but commenting out that section for now

--- js/main.js
+++ js/main.js
@@ -3,9 +3,38 @@
 
     var $body = $('body');
 
-    $body.on('click', '#mobile-nav', function() {
-      $(this).toggleClass('open');
-    });
+    $body
+      .on('click', '#mobile-nav', function() {
+        $(this).toggleClass('open');
+      })
+      .on('click', 'a.modal', function(e) {
+        e.stopPropagation();
+        var id = $(this).attr('href');
+        var $modal = $('aside.modal' + id);
+
+        if ($modal.length) {
+          $body.addClass('modal-open');
+          $modal.addClass('open');
+        }
+      })
+      .on('click', '.modal .close', function() {
+        closeModal();
+      })
+      .on('click', function(e) {
+        if ($body.hasClass('modal-open')) {
+          var $target = $(e.target);
+          var modalTargeted = $target.hasClass('modal') || $target.closest('.modal').length;
+          if (!modalTargeted) {
+            closeModal();
+          }
+        }
+      })
+    ;
+
+    function closeModal() {
+      $('body').removeClass('modal-open');
+      $('aside.modal').removeClass('open');
+    }
 
 	});
 })(jQuery);
